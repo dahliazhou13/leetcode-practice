ee9a9007ba62c9f1ec49a816cb5b90bd6077b745
Doris Zhou <zhoudoris13@gmail.com>
2021-03-10 23:49:00 -0500

    added extra description

--- ladderLength.java
+++ ladderLength.java
@@ -1,3 +1,16 @@
+/**
+ * Description:
+ * A transformation sequence from word beginWord to word endWord 
+ * using a dictionary wordList is a sequence of words such that:
+
+The first word in the sequence is beginWord.
+The last word in the sequence is endWord.
+Only one letter is different between each adjacent pair of words
+in the sequence.
+Every word in the sequence is in wordList.
+Given two words, beginWord and endWord, and a dictionary wordList, 
+return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+ */
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
        
